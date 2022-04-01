0dcfd784a81e62ffddc7b7f9f4aa0fd66611a6fb
Su4a <stSu4a@gmail.com>
2022-04-01 13:51:24 +0300

    Java 2 HM 2

---
+++ Java/Java 2/Lesson 2/CalculatorArr.java
@@ -0,0 +1,30 @@
+public class CalculatorArr {
+
+    public boolean checkArrFoSize(String[][] arr, int arrLenght) {
+        if (arr.length != arrLenght) {
+            throw new MyArraySizeException("Размер массива не равен 4");
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != arrLenght) {
+                throw new MyArraySizeException("Размер массива не равен 4");
+            }
+        }
+        return true;
+    }
+
+    public int sumArr(String[][] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    String message = String.format("В массиве [%s][%s] содержится не число", i , j);
+                    throw new MyArrayDataException(message);
+                }
+            }
+        }
+        return sum;
+    }
+}
